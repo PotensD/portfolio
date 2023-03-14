1529d0c16214bdf8390ee8f6bbe80f13abe6ef6c
dinhdjj <dinhdjj@gmail.com>
2023-03-14 15:19:22 +0700

    feat: recent posts section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import IntroductorySection from './IntroductorySection'
+import RecentPostSection from './RecentPostSection'
 
 export default function Home() {
   return (
     <>
       <IntroductorySection className='pt-16' />
+      <RecentPostSection className='pt-32' />
     </>
   )
 }
