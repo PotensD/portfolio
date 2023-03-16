4ca38282f596eabb9a1f417d79bd61b12407e6a1
dinhdjj <dinhdjj@gmail.com>
2023-03-16 15:30:45 +0700

    feat: posts section

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,11 @@
 import IntroductorySection from './IntroductorySection'
+import PostsSection from './PostsSection'
 
 export default function Posts() {
   return (
     <>
       <IntroductorySection className='pt-16' />
+      <PostsSection className='pt-32' />
     </>
   )
 }
