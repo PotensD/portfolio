d259e7f4a48ba6194e78bf977c864f501b383bc5
PotensD <me@PotensD.dev>
2023-03-27 20:28:32 +0700

    chore: update copyright name & use TypeAnimation (#6)

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -51,7 +51,7 @@ export default function Footer({ className }: { className?: string }) {
         </div>
         <div className='mt-8 md:order-1 md:mt-0'>
           <p className='text-center text-xs leading-5 text-gray-500'>
-            &copy; 2023 dinhdjj. All rights reserved.
+            &copy; 2023 PotensD. All rights reserved.
           </p>
         </div>
       </div>
