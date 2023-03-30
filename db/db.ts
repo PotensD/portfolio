3d129a89f50558ce4a52168041a33ca4cba4b70a
PotensD <me@PotensD.dev>
2023-03-30 15:16:06 +0700

    chore: change planetscale env

--- db/db.ts
+++ db/db.ts
@@ -2,9 +2,9 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
 
 const connection = connect({
-  host: process.env['DATABASE_HOST'],
-  username: process.env['DATABASE_USERNAME'],
-  password: process.env['DATABASE_PASSWORD'],
+  host: process.env['PLANETSCALE_DB_HOST'],
+  username: process.env['PLANETSCALE_DB_USERNAME'],
+  password: process.env['PLANETSCALE_DB_PASSWORD'],
 });
 
 export const db = drizzle(connection);
