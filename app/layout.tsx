56e57b1b14cd3fd41365deb5559e12e085b0a04e
PotensD <me@PotensD.dev>
2023-04-04 10:03:27 +0700

    chore: install vercel-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from './Footer'
 import Header from './Header'
 import './globals.css'
 import TrpcClientProvider from './trpc'
+import { Analytics } from '@vercel/analytics/react';
 import { Be_Vietnam_Pro } from 'next/font/google'
 
 export const metadata = {
@@ -44,15 +45,16 @@ export default function RootLayout({
 		<TrpcClientProvider>
 			<html lang='en' className={`${beVietnamPro.variable}`}>
 				<body
-					className={`${
-						isDev ? 'debug-screens' : ''
-					} bg-slate-900 bg-[url('/background-grid-60.svg')] bg-fixed bg-[center_top_15%] bg-no-repeat text-slate-400 md:bg-[left_15%_top_15%]`}
+					className={`${isDev ? 'debug-screens' : ''
+						} bg-slate-900 bg-[url('/background-grid-60.svg')] bg-fixed bg-[center_top_15%] bg-no-repeat text-slate-400 md:bg-[left_15%_top_15%]`}
 				>
 					<div className='container mx-auto px-4 py-6 md:px-8 xl:px-12'>
 						<Header />
 						{children}
 						<Footer className='mt-48' />
 					</div>
+
+					<Analytics />
 				</body>
 			</html>
 		</TrpcClientProvider>
